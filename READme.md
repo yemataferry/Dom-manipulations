dc3d972c455c39ee763f551b6144e2d6a87c4b67
Yemata Ferry <yemataferry@gmail.com>
2023-12-13 08:10:34 +0100

    js and css done

---
+++ READme.md
@@ -0,0 +1,21 @@
+# DOM MANIPULATION
+## ABOUT
+    The project is about an App capable of Researching and Filtering the users information from the database with the possibility of Deleting it or Not.
+
+## How to Open and run the App
+    Using the proper link 'https://yemataferry.github.io/palindrome' open the page your browser. The page open will display a Dialogbox where a place 'Name' and 'Age' will be allocated for the users-Name and Age needed you wish to check.
+    After entering your word in the space provided, click on the 'Submit' button to run and the result will be display under in a table classified under Initials , Name and Age.  
+![DOM](./assets/images/desktop%20view.png)
+## Project Technical
+### Built with
+    HTML
+    CSS
+    JavaScript
+### Designs
+    project is mobile responsive first design and @mediaquery fully responsive
+![mobile responsivenes](./assets/images/phone-view.png)
+## Live site
+  [Palindrome-site](https://yemataferry.github.io/palindrome/)
+## Author
+     **Yemata Ferry**
+     - GitHub: [@yemataferry](https://github.com/yemataferry/palindrome/pull/1)
---
+++ assets/scripts/index.js
@@ -0,0 +1,129 @@
+// creating inputs and button
+
+const boxes = document.getElementById("container");
+const form = document.querySelector('form')
+
+
+const name = document.createElement('input')
+const age = document.createElement('input')
+const submit = document.createElement('button')
+
+name.type = 'text'
+age.type = 'Number'
+
+name.classList.add('noms')
+age.classList.add('ages')
+submit.classList.add('submits')
+
+name.placeholder = 'Name'
+age.placeholder = 'Age'
+submit.innerHTML = 'Submit'
+
+boxes.appendChild(name)
+boxes.appendChild(age)
+boxes.appendChild(submit)
+
+
+const base = document.getElementById('database')
+let table = document.createElement('table')
+
+let Data = [
+  { name: 'Jack Baroski', age: 28 },
+  { name: 'Boris Mao', age: 20 },
+  { name: 'Joe Dan', age: 30 },
+  { name: 'Ferry Michaelson', age: 25 },
+  { name: 'Ramy Sanchez', age: 19 }
+]
+console.log(Data)
+
+// fxn creating the initials of each name 
+
+function initials(string) {
+  let initial = string[0]
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (string[i] === ' ') {
+      initial += '.' + string[i + 1]
+    }
+  }
+  return initial.toUpperCase();
+}
+// console.log(initials(Data))
+
+function researchList(user) {
+  // const btnId = user.split(' ').join('-')
+  return `
+  <tr> 
+      <td>${initials(user.name)}</td>
+      <td>${user.name} </td>
+      <td>${user.age} </td>
+      <td><button id="${user.name}" class="delete-btn">Delete</button></td>
+  </tr>`;
+}
+
+// displaying the headers so as to create it back anytime the page will be refresh 
+
+function displayList(Data) {
+  base.innerHTML = `
+    <tr>
+        <th>Initials</th>
+        <th>Name</th>
+        <th>Age</th>
+    </tr>
+  `
+  Data.forEach(function (user, i) {
+    base.innerHTML += researchList(user)
+  });
+  userDelete()
+}
+
+// The research function
+
+const research = (checkName, checkAge) => {
+  const result = Data.filter((user) => {
+    let researchName = checkName.toLowerCase()
+    let userName = user.name.toLowerCase()
+
+    if ((researchName && userName.includes(researchName)) || (checkAge && user.age === checkAge)) {
+      return true
+    }
+  })
+  return result
+}
+
+// noms.addEventListener('input', research(a, b))
+// age.addEventListener('input', research(a, b))
+
+displayList(Data)
+
+// Applying the function to the submit button 
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const nam = name.value
+  const ag = parseInt(age.value)
+  const res = research(nam, ag)
+  console.log(nam, ag, res)
+  displayList(res)
+})
+
+// Deleting fxn to delete desired user
+
+function userDelete() {
+  const deleteButtons = document.querySelectorAll('.delete-btn')
+
+  deleteButtons.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      const userName = event.target.id
+
+      const update = Data.filter((user) => {
+        if (user.name !== userName) {
+          return true
+        }
+      })
+      console.log(update, userName)
+      Data = update
+      displayList(update)
+    })
+  })
+}
+userDelete()
